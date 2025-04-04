08a5da3a0c08f5a1fcaccadc086a18eebe17a183
heiwa4126 <heiwa4126@gmail.com>
2025-04-04 16:29:52 +0900

    update

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,12 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
 
 export default [
 	index("routes/home.tsx"),
 	route("page1", "routes/page1.tsx"),
 	route("page2", "routes/page2.tsx"),
-	route("products", "routes/products/home.tsx"),
-	route("products/:id", "routes/products/products.tsx"),
+	...prefix("products", [
+		index("routes/products/home.tsx"),
+		route(":id", "routes/products/products.tsx"),
+	]),
 	// route("*", "routes/notFound.tsx"),
 ] satisfies RouteConfig;
