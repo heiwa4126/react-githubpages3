af68828c8eb9c12fa9d987100f8b1a2fde0ffa45
heiwa4126 <heiwa4126@gmail.com>
2025-04-04 17:40:11 +0900

    update

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,16 @@
 import type { Config } from "@react-router/dev/config";
 
+import products from "app/assets/products.json";
+import type { Products } from "app/routes/products/types";
+
+const productsKeys = Object.keys(products as Products) as Array<keyof Products>;
+
 export default {
 	// Config options...
 	// Server-side render by default, to enable SPA mode set this to `false`
 	ssr: false,
 	async prerender() {
-		return ["/", "/page1", "/products", "/products/aaa"];
+		const productPages = productsKeys.map((key) => `/products/${key}`);
+		return ["/", "/page1", "/products", ...productPages];
 	},
 } satisfies Config;
